3614407b5450cf785db5af6ed3537aee9508724e
mjmonarch <mjmonarch@gmail.com>
2021-12-11 23:08:23 +0200

    stage 1 completed!

--- Honest Calculator/task/honest_calc.py
+++ Honest Calculator/task/honest_calc.py
@@ -1,1 +1,21 @@
-# write your code here
+# ------------------------------STAGE 1----------------------------------------------
+msg_0 = "Enter an equation"
+msg_1 = "Do you even know what numbers are? Stay focused!"
+msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
+
+while True:
+    print(msg_0)
+    operation_string = input()
+    operation_list = operation_string.split(sep=" ", maxsplit=3)
+    x = operation_list[0]
+    oper = operation_list[1]
+    y = operation_list[2]
+    try:
+        float(x) and float(y)
+    except:
+        print(msg_1)
+        continue
+    if not (oper in ['+', '-', '*', '/']):
+        print(msg_2)
+        continue
+    break
