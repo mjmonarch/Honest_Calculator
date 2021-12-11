90b3f4beb236cb437d5a667c9f2c5ddac5cc8d29
mjmonarch <mjmonarch@gmail.com>
2021-12-11 23:49:42 +0200

    stage 3 completed!

--- Honest Calculator/task/honest_calc.py
+++ Honest Calculator/task/honest_calc.py
@@ -20,19 +20,64 @@
 #         continue
 #     break
 
-# ------------------------------STAGE 2----------------------------------------------
+# # ------------------------------STAGE 2----------------------------------------------
+# msg_0 = "Enter an equation"
+# msg_1 = "Do you even know what numbers are? Stay focused!"
+# msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
+# msg_3 = "Yeah... division by zero. Smart move..."
+#
+# while True:
+#     print(msg_0)
+#     operation_string = input()
+#     operation_list = operation_string.split(sep=" ", maxsplit=3)
+#     try:
+#         x = float(operation_list[0])
+#         y = float(operation_list[2])
+#     except:
+#         print(msg_1)
+#         continue
+#     oper = operation_list[1]
+#     if not (oper in ['+', '-', '*', '/']):
+#         print(msg_2)
+#         continue
+#     if oper == '+':
+#         result = x + y
+#     elif oper == '-':
+#         result = x - y
+#     elif oper == "*":
+#         result = x * y
+#     elif oper == "/" and y == 0:
+#         print(msg_3)
+#         continue
+#     else:
+#         result = x / y
+#     print(result)
+#     break
+
+# ------------------------------STAGE 3----------------------------------------------
 msg_0 = "Enter an equation"
 msg_1 = "Do you even know what numbers are? Stay focused!"
 msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
 msg_3 = "Yeah... division by zero. Smart move..."
+msg_4 = "Do you want to store the result? (y / n):"
+msg_5 = "Do you want to continue calculations? (y / n):"
 
+memory = 0.0
+finished = True
 while True:
     print(msg_0)
     operation_string = input()
     operation_list = operation_string.split(sep=" ", maxsplit=3)
+    x = operation_list[0]
+    oper = operation_list[1]
+    y = operation_list[2]
+    if x == 'M':
+        x = memory
+    elif y == 'M':
+        y = memory
     try:
-        x = float(operation_list[0])
-        y = float(operation_list[2])
+        x = float(x)
+        y = float(y)
     except:
         print(msg_1)
         continue
@@ -52,4 +97,23 @@ while True:
     else:
         result = x / y
     print(result)
-    break
+    while True:
+        print(msg_4)
+        answer = input()
+        if answer == 'y':
+            memory = result
+        elif answer != 'n':
+            continue
+        break
+    while True:
+        print(msg_5)
+        answer = input()
+        if answer == 'y':
+            finished = False
+            break
+        elif answer != 'n':
+            continue
+        finished = True
+        break
+    if finished:
+        break
